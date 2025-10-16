e075f2194e8057d9223de9dde92a373f9a6aa186
hojiakbar <hojiakbarsobirov30@gmail.com>
2025-10-16 18:42:12 +0500

    update bonusPage

--- src/components/BonusPage.jsx
+++ src/components/BonusPage.jsx
@@ -5,29 +5,37 @@ const BonusPage = () => {
   return (
     <section className="relative w-full min-h-[60vh] flex flex-col justify-center items-center overflow-hidden text-center px-4">
       {/* Animated background */}
-      <div className="absolute inset-0  animate-gradient"></div>
+      <div className="absolute inset-0 animate-gradient"></div>
 
       {/* Content */}
-      <div data-aos="fade-up" className="relative z-10 flex flex-col md:flex-row justify-center items-center gap-8 md:gap-14 text-white">
+      <div
+        data-aos="fade-up"
+        className="relative z-10 flex flex-col md:flex-row justify-center items-center gap-5 md:gap-10 text-white"
+      >
         <img
           src="/book3.png"
           alt="Bonus book"
-          className="w-[250px] sm:w-[300px] md:w-[380px] lg:w-[420px] drop-shadow-lg"
+          className="w-[220px] sm:w-[280px] md:w-[350px] lg:w-[400px] drop-shadow-lg"
         />
-        <p className="font-medium text-lg sm:text-xl md:text-2xl leading-relaxed md:w-[400px] lg:w-[550px]">
+        <p className="font-medium text-lg sm:text-xl md:text-2xl leading-relaxed md:w-[380px] lg:w-[520px]">
           24 soat ichida <br /> kursga yoziling va <br /> “Ko‘chada gaplashamiz”
-          audio kitobini <br />
+          audio{" "}
+          <span
+            role="img"
+            aria-label="headphones"
+            className="inline-block text-yellow-300 text-2xl align-middle ml-1"
+          >
+            🎧
+          </span>{" "}
+          kitobini <br />
           <span className="font-semibold text-yellow-300">
             bepul qo‘lga kiriting!
           </span>
+          <br />
+          <Link target="_blank" to="https://t.me/Ruspeak_admin">
+            <button className="beautiful-button mt-3">Bonus !</button>
+          </Link>
         </p>
-      </div>
-
-      {/* Button */}
-      <div className="relative z-10 mt-8">
-        <Link target="_blank" to="https://t.me/Ruspeak_admin">
-          <button class="beautiful-button">Bonus !</button>
-        </Link>
       </div>
     </section>
   );
